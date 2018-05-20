ee424e75f8cce097a547ec37b221e304c7f61a67
Snob <vvvaaa98@mail.ru>
2018-05-20 21:22:58 +0300

    14
    14

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,61 +1,29 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) {
-
-
-int x = 2;
-        int y =2;
-
-
-        System.out.println(2==2 && true);
-
-        int c =    x< y ? 2 :y;
-
-
-        String qw = "qweqwe";
-        String v = qw.substring(0,3);
-        System.out.println(v);
-
-        String j ="qwe";
-        String d = "zxc";
-
-        String k = j+" "+ d;
-
-        System.out.println(k);
-String  jj = String.join(" ", "a","b","c","d");
-        System.out.println(jj);
-j.equals("q");
-j.equals("");
-String qwe= "";
-j.length();
-j.charAt(2);
-;
-        System.out.println(j.toUpperCase());
-System.out.println(j.replace("w","o")); System.out.println(j.indexOf("q"));
-
-StringBuilder st = new StringBuilder();
-st.append("qweqwe");
-st.append("zzx");
-        System.out.println(st);
-      String  nst =   st.toString();
-        System.out.println(1+1); st.append(1).append(2).append("123");
-
-st.insert(0,"1");
-        System.out.println(st);
-
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        int  m = sc.nextInt();
 
-        System.out.println(m);
-        System.out.println(sc.nextLine());
+int ch = sc.nextInt();
 
-        }
+switch (ch){
 
+    case 1 :
+        System.out.println(1);
+                break;
+    case 2 :
+        System.out.println(2);
+                break;
+}
 
     }
+}
