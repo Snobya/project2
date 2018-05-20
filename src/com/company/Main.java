bc2e5f1412f749febedb48231cdc1a77344bde51
Snob <vvvaaa98@mail.ru>
2018-05-20 16:27:18 +0300

    8
    8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,44 @@ public class Main {
     public static void main(String[] args) {
 
 
-int a = 2;
-int c =2;
-c = a++;
+int x = 2;
+        int y =2;
 
-   //     System.out.println((c=a++));
-        System.out.println(c);
-        System.out.println(a&&b);
+
+        System.out.println(2==2 && true);
+
+        int c =    x< y ? 2 :y;
+
+
+        String qw = "qweqwe";
+        String v = qw.substring(0,3);
+        System.out.println(v);
+
+        String j ="qwe";
+        String d = "zxc";
+
+        String k = j+" "+ d;
+
+        System.out.println(k);
+String  jj = String.join(" ", "a","b","c","d");
+        System.out.println(jj);
+j.equals("q");
+j.equals("");
+String qwe= "";
+j.length();
+j.charAt(2);
+;
+        System.out.println(j.toUpperCase());
+System.out.println(j.replace("w","o")); System.out.println(j.indexOf("q"));
+
+StringBuilder st = new StringBuilder();
+st.append("qweqwe");
+st.append("zzx");
+        System.out.println(st);
+
+      String  nst =   st.toString();
+        System.out.println(1+1);
         }
+
+
     }
