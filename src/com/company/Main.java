dbd06bb95d0fbf0ce92d56e231f8f985545350d5
Snob <vvvaaa98@mail.ru>
2018-05-20 14:17:39 +0300

    2
    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,70 @@
 package com.company;
 
+import static java.lang.Math.*;
+
+/**
+ * @name
+ * @asd
+ * asdasd
+ * adsasd@ sadfsdf@asdf @sadfsdf
+ * @author  V
+ * asdad
+ */
 public class Main {
+    static final int nm = 8;
+
+
+    double wqeq =10;
 
     public static void main(String[] args) {
-	// write your code here
+
+        String  greetinf = "simple string";
+        System.out.println(greetinf);
+        for (int i = 0; i < greetinf.length(); i++) {
+            System.out.println("=");
+            System.out.println();
+
+            System.out.println("1111111111111 " + 15%2);
+
+            Math.sqrt(12);
+            System.out.println(            sqrt(12));
+int a = 123123;
+int b = 123;
+int qweqwe, j ;
+
+int l;
+l=5;
+final int QWEQWEQWE =7;
+
+
+Math.cos();
+
+
+
+            System.out.println(l);
+final int q = 123;
+
+
+Character.isJavaIdentifierStart('#');
+            System.out.println(Character.isJavaIdentifierStart('#'));
+long  c = -123123l;
+
+
+int eqr = 0123123;
+
+
+
+
+        }
     }
+
+
+    //
+
+    /*
+    ada
+    as
+    d
+    asd
+     */
 }
